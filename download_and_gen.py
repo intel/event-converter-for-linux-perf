de4f4a7f0b45073b375541bce84ac609c24a72a2
Ian Rogers <irogers@google.com>
2022-06-30 22:41:26 -0700

    Rewrite download_and_gen.sh in python
    Use mapfile.csv for the version numbers and directly download the files
    while generating perf json, rather than downloading then working on
    files.
    Add an ability to generate a perf mapfile.csv.

---
+++ download_and_gen.py
@@ -0,0 +1,274 @@
+#!/usr/bin/python
+import argparse
+import collections
+import csv
+import io
+import json
+import os
+import re
+import uncore_csv_json
+import urllib.request
+import importlib
+from itertools import takewhile
+from typing import (Dict, DefaultDict, Sequence, Set)
+
+json_to_perf_json = importlib.import_module('json-to-perf-json')
+hybrid_json_to_perf_json = importlib.import_module('hybrid-json-to-perf-json')
+extract_tma_metrics = importlib.import_module('extract-tma-metrics')
+
+
+class Model:
+    shortname: str
+    longname: str
+    version: str
+    files: Dict[str, str]
+    models: Sequence[str]
+
+    def __init__(self, shortname: str, longname: str, version: str,
+                 models: Set[str], files: Dict[str, str]):
+        self.shortname = shortname
+        self.longname = longname.lower()
+        self.version = version
+        self.models = sorted(models)
+        self.files = files
+
+    def __lt__(self, other):
+        # Sort by model number: min(self.models) < min(other.models)
+        return self.longname < other.longname
+
+    def __str__(self):
+        return f'{self.shortname} / {self.longname}\n\tmodels={self.models}\n\t' + '\n\t'.join(
+            [f'{type}_url = {url}' for (type, url) in self.files.items()])
+
+    def to_perf_json(self, outdir: str):
+        # Core event files.
+        if 'atom' in self.files:
+            with urllib.request.urlopen(self.files['atom']) as atom_json:
+                with urllib.request.urlopen(self.files['core']) as core_json:
+                    hybrid_json_to_perf_json.hybrid_json_to_perf_json(
+                        atom_json, core_json, outdir)
+        else:
+            with urllib.request.urlopen(self.files['core']) as core_json:
+                json_to_perf_json.json_to_perf_json(core_json, outdir, '')
+
+        # Uncore event files.
+        if 'uncore' in self.files:
+            uncore_csv_file = f'perf-uncore-events-{self.shortname.lower()}.csv'
+            if os.path.exists(uncore_csv_file):
+                uncore_csv = open(uncore_csv_file, 'r')
+            else:
+                uncore_csv = io.StringIO('')
+            with urllib.request.urlopen(self.files['uncore']) as uncore_json:
+                if 'uncore experimental' in self.files:
+                    with urllib.request.urlopen(
+                            self.files['uncore experimental']
+                    ) as experimental_json:
+                        uncore_csv_json.uncore_csv_json(
+                            csvfile=uncore_csv,
+                            jsonfile=uncore_json,
+                            extrajsonfile=experimental_json,
+                            targetdir=outdir,
+                            all_events=True)
+                else:
+                    uncore_csv_json.uncore_csv_json(
+                        csvfile=uncore_csv,
+                        jsonfile=uncore_json,
+                        extrajsonfile=None,
+                        targetdir=outdir,
+                        all_events=True)
+        # TMA metrics.
+        tma_cpu = extract_tma_metrics.find_tma_cpu(self.shortname)
+        if not tma_cpu:
+            return
+        with urllib.request.urlopen(self.files['tma metrics']) as tma_metrics:
+            tma_metrics_lines = [
+                l.decode('utf-8') for l in tma_metrics.readlines()
+            ]
+            outfile = open(
+                f'{outdir}/{self.shortname.replace("-","").lower()}-metrics.json',
+                'w')
+            if 'atom' in self.files:
+                core_json = io.StringIO()
+                extract_tma_metrics.extract_tma_metrics(
+                    csvfile=tma_metrics_lines,
+                    cpu=tma_cpu,
+                    extrajson=None,
+                    cstate=False,
+                    extramodel=self.shortname,
+                    unit='cpu_core',
+                    expr_events='100',
+                    memory=True,
+                    verbose=False,
+                    outfile=core_json)
+                atom_json = io.StringIO()
+                with urllib.request.urlopen(
+                        self.files['e-core tma metrics']) as e_core_tma_metrics:
+                    e_core_tma_metrics_lines = [
+                        l.decode('utf-8')
+                        for l in e_core_tma_metrics.readlines()
+                    ]
+                    e_core_tma_cpu = {
+                        'ADL': 'GRT',
+                    }[self.shortname]
+                    extract_tma_metrics.extract_tma_metrics(
+                        csvfile=e_core_tma_metrics_lines,
+                        cpu=e_core_tma_cpu,
+                        extrajson=None,
+                        cstate=True,
+                        extramodel=e_core_tma_cpu,
+                        unit='cpu_atom',
+                        expr_events='100',
+                        memory=True,
+                        verbose=False,
+                        outfile=atom_json)
+                jo = json.loads(core_json.getvalue())
+                for event in json.loads(atom_json.getvalue()):
+                    jo.append(event)
+                outfile.write(
+                    json.dumps(
+                        jo, sort_keys=True, indent=4, separators=(',', ': ')))
+                outfile.write('\n')
+            else:
+                extract_tma_metrics.extract_tma_metrics(
+                    csvfile=tma_metrics_lines,
+                    cpu=tma_cpu,
+                    extrajson=None,
+                    cstate=True,
+                    extramodel=self.shortname,
+                    unit='',
+                    expr_events='100',
+                    memory=True,
+                    verbose=False,
+                    outfile=outfile)
+
+    def mapfile_line(self):
+        if len(self.models) == 1:
+            ret = min(self.models)
+        else:
+            prefix = ''.join(
+                c[0] for c in takewhile(lambda x: all(x[0] == y for y in x
+                                                     ), zip(*self.models)))
+            if len(min(self.models)) - len(prefix) > 1:
+                start_bracket = '('
+                end_bracket = ')'
+                seperator = '|'
+            else:
+                start_bracket = '['
+                end_bracket = ']'
+                seperator = ''
+            ret = prefix + start_bracket
+            first = True
+            for x in self.models:
+                if not first:
+                    ret += seperator
+                ret += x[len(prefix):]
+                first = False
+            ret += end_bracket
+        ret += f',{self.version.lower()},{self.longname},core'
+        return ret
+
+
+class Mapfile:
+    archs: Sequence[Model]
+
+    def __init__(self, base_url: str):
+        self.archs = []
+        longnames: Dict[str, str] = {}
+        models: DefaultDict[str, Set[str]] = collections.defaultdict(set)
+        files: Dict[str, Dict[str, str]] = collections.defaultdict(dict)
+        versions: Dict[str, str] = {}
+        with urllib.request.urlopen(base_url + '/mapfile.csv') as mapfile_csv:
+            mapfile_csv_lines = [
+                l.decode('utf-8') for l in mapfile_csv.readlines()
+            ]
+            mapfile = csv.reader(mapfile_csv_lines)
+            first_row = True
+            for (family_model, version, path, event_type, core_type,
+                 native_model_id, core_role_name) in mapfile:
+                if first_row:
+                    assert family_model == 'Family-model'
+                    assert version == 'Version'
+                    assert path == 'Filename'
+                    assert event_type == 'EventType'
+                    assert core_type == 'Core Type'
+                    assert native_model_id == 'Native Model ID'
+                    assert core_role_name == 'Core Role Name'
+                    first_row = False
+                    continue
+                shortname = re.sub(r'/(.*)/.*', r'\1', path)
+                longname = re.sub(rf'/{shortname}/([^_]*)_.*', r'\1', path)
+                url = base_url + path
+
+                # Bug fixes:
+                if shortname == 'SNR' and event_type == 'uncore' and 'experimental' in path:
+                    event_type = 'uncore experimental'
+
+                # Workarounds:
+                if event_type == 'hybridcore':
+                    event_type = 'core' if core_role_name == 'Core' else 'atom'
+                if shortname == 'KNM':
+                    # The files for KNL and KNM are the same as are
+                    # the longnames. We don't want the KNM shortname
+                    # but do want the family_model.
+                    models['KNL'].add(family_model)
+                    continue
+
+                if not shortname in longnames:
+                    longnames[shortname] = longname
+                else:
+                    assert longnames[shortname] == longname
+                if not shortname in versions:
+                    versions[shortname] = version
+                else:
+                    assert versions[shortname] == version
+                models[shortname].add(family_model)
+                if shortname in files and event_type in files[shortname]:
+                    assert files[shortname][event_type] == url, \
+                        f'Expected {shortname}/{longname} to have just 1 {event_type} url {files[shortname][event_type]} but found {url}'
+                else:
+                    files[shortname][event_type] = url
+
+        for (shortname, longname) in longnames.items():
+            files[shortname]['tma metrics'] = base_url + '/TMA_Metrics-full.csv'
+            if 'atom' in files[shortname]:
+                files[shortname][
+                    'e-core tma metrics'] = base_url + '/E-core_TMA_Metrics.csv'
+            self.archs += [
+                Model(shortname, longname, versions[shortname],
+                      models[shortname], files[shortname])
+            ]
+        self.archs.sort()
+
+    def __str__(self):
+        result = ''
+        for model in self.archs:
+            result += str(model) + '\n'
+        return result
+
+    def to_perf_json(self, outdir: str):
+        gen_mapfile = open(f'{outdir}/mapfile.csv', 'w')
+        for model in self.archs:  #[x for x in self.archs if x.shortname == 'TGL']:
+            print(f'Generating json for {model.longname}')
+            modeldir = outdir + '/' + model.longname
+            os.system(f'mkdir -p {modeldir}')
+            model.to_perf_json(modeldir)
+            gen_mapfile.write(model.mapfile_line() + '\n')
+
+
+def generate_all_event_json(url: str, outdir: str):
+    mapfile = Mapfile(url)
+
+    mapfile.to_perf_json(outdir)
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--url', default='https://download.01.org/perfmon')
+    ap.add_argument('--outdir', default='perf2')
+    args = ap.parse_args()
+
+    generate_all_event_json(args.url, args.outdir)
+
+
+if __name__ == '__main__':
+    main()
